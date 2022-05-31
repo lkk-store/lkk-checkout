56ea5de770355b1cd7ea5f70b614753b1ea2e979
KK Lai <kklai>
2022-06-01 00:17:04 +0800

    Fiels

---
+++ server.js
@@ -0,0 +1,55 @@
+// This is your test secret API key.
+const stripe = require('stripe')('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');
+// const stripe = require('stripe')('sk_test_51L1oSlLWOuW7oqJ6eeycTRYmPeAW7i0LXkX8gtPMMSXhSOAkRAhHyRby1qYTYJdNKG9Oc6077P5AaLTihpwDWkap00YuXfrFsp')
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.use(express.urlencoded());
+app.use(express.json());      // if needed
+
+const YOUR_DOMAIN = 'http://localhost:4242';
+
+app.post('/create-checkout-session', async (req, res) => {
+
+  var keys = Object.keys(req.body);
+
+  var lookup = {
+    tshirt360: 'price_1L1p4pLWOuW7oqJ6xbmkJASg',
+    tshirt320: 'price_1L58s9LWOuW7oqJ67uvMmcPM',
+    sticker: 'price_1L5QeALWOuW7oqJ6pgWR5TkG'
+  }
+
+  // var lookup = {
+  //   tshirt360: "price_1L59VcLWOuW7oqJ6GLLyHMc3",
+  //   tshirt320: "price_1L5WwsLWOuW7oqJ6qdmGFsA3",
+  //   sticker: "price_1L5WxNLWOuW7oqJ6OmH7EaBw"
+  // }
+
+  var line_items = [];
+  keys.forEach(function(d){
+    if (+req.body[d] > 0) {
+      line_items.push({
+        price: lookup[d],
+        quantity: +req.body[d]
+      })
+    }
+  })
+
+  console.log(line_items)
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: line_items,
+    phone_number_collection: {
+        enabled: true,
+      },
+    mode: 'payment',
+    success_url: `${YOUR_DOMAIN}/success.html`,
+    cancel_url: "https://www.lkk-store.com",
+    automatic_tax: {enabled: false},
+  });
+
+  res.redirect(303, session.url);
+});
+
+app.listen(4242, () => console.log('Running on port 4242'));
