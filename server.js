a9a8044be87090957a5843ef2f2e26de5dacc657
KK Lai <kklai>
2022-06-01 11:56:18 +0800

    fix error

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());      // if needed
 
 const YOUR_DOMAIN = 'http://localhost:4242';
